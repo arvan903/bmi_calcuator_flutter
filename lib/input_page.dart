ade015768d3d2e2cd63f1e7440ed01e7e029a955
arvan903 <arvan903@hotmail.com>
2022-03-19 22:09:11 +0330

    in progress 20%

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
 const bottomContainerColour = Color(0xFFEB1555);
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, required this.cardChild});
-  final Color colour;
-  final Widget cardChild;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10),
-      ),
-    );
-  }
-}
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -49,13 +34,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
-                    cardChild: null,
+                    cardChild: IconContet(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
-                    cardChild: null,
+                    cardChild: IconContet(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
                   ),
                 )
               ],
@@ -66,9 +57,7 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: null,
-                  ),
+                      colour: activeCardColour, cardChild: Text('hi')),
                 ),
               ],
             ),
@@ -78,15 +67,11 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: null,
-                  ),
+                      colour: activeCardColour, cardChild: Text('hi')),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColour,
-                    cardChild: null,
-                  ),
+                      colour: activeCardColour, cardChild: Text('hi')),
                 )
               ],
             ),
