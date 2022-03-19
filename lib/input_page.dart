e910f876ce10ec26164275ea562a5d77fc77167a
arvan903 <arvan903@hotmail.com>
2022-03-19 17:28:32 +0330

    Functionalist

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+class ReusableCard extends StatelessWidget {
+  ReusableCard(this.colour);
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -11,77 +27,51 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'BMI CalCulator',
-              style: TextStyle(fontFamily: 'Georgia'),
-            ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'BMI CalCulator',
+            style: TextStyle(fontFamily: 'Georgia'),
           ),
         ),
-        body: Column(
-          children: [
-            Expanded(
-                child: Row(
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 )
               ],
-            )),
-            Expanded(
-                child: Row(
+            ),
+          ),
+          Expanded(
+            child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                )
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
               ],
-            )),
-            Expanded(
-                child: Row(
+            ),
+          ),
+          Expanded(
+            child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 )
               ],
-            )),
-          ],
-        ));
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
