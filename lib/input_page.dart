242d1f099d00e711ad86aa6dd4d6a6b327f3838b
arvan903 <arvan903@hotmail.com>
2022-03-20 18:10:10 +0330

    New Dard File added for more customise, some functional

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inActiveCardColour = Color(0xFF111328);
-const bottomContainerColour = Color(0xFFEB1555);
 enum GenderType { male, female }
 
 class InputPage extends StatefulWidget {
@@ -44,8 +41,8 @@ class _InputPageState extends State<InputPage> {
                     },
                     child: ReusableCard(
                       colour: selectedGender == GenderType.male
-                          ? activeCardColour
-                          : inActiveCardColour,
+                          ? kActiveCardColour
+                          : kInActiveCardColour,
                       cardChild: IconContet(
                         icon: FontAwesomeIcons.mars,
                         label: 'MALE',
@@ -62,8 +59,8 @@ class _InputPageState extends State<InputPage> {
                     },
                     child: ReusableCard(
                       colour: selectedGender == GenderType.female
-                          ? activeCardColour
-                          : inActiveCardColour,
+                          ? kActiveCardColour
+                          : kInActiveCardColour,
                       cardChild: IconContet(
                         icon: FontAwesomeIcons.venus,
                         label: 'FEMALE',
@@ -75,34 +72,31 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                      colour: activeCardColour, cardChild: Text('hi')),
-                ),
-              ],
+              child: ReusableCard(
+            colour: kActiveCardColour,
+            cardChild: Column(
+              children: [Text('HEIGHT')],
             ),
-          ),
+          )),
           Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
-                      colour: activeCardColour, cardChild: Text('hi')),
+                      colour: kActiveCardColour, cardChild: Text('hi')),
                 ),
                 Expanded(
                   child: ReusableCard(
-                      colour: activeCardColour, cardChild: Text('hi')),
+                      colour: kActiveCardColour, cardChild: Text('hi')),
                 )
               ],
             ),
           ),
           Container(
-            color: bottomContainerColour,
+            color: kBottomContainerColour,
             margin: EdgeInsets.only(top: 10),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           )
         ],
       ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -4,14 +4,17 @@ class ReusableCard extends StatelessWidget {
   ReusableCard({required this.colour, required this.cardChild});
   final Color colour;
   final Widget cardChild;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
